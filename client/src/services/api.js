92bf41449c3f1973eb667ea62de2322fe538a39a
akshithteja <sunnyveeramalla0@gmail.com>
2026-03-30 23:00:36 +0530

    Merge Render fixes into main branch

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:8085/api";
 const API = axios.create({
-    baseURL: "http://localhost:8085/api"
+    baseURL: apiBaseUrl.endsWith("/api") ? apiBaseUrl : `${apiBaseUrl}/api`
 });
 
 export default API;
